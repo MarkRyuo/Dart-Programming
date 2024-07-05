51e00ff3ae474a8dd6064e118980952f9229f65b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-05 09:12:54 +0800

    update here

--- Day 1/app2.dart
+++ Day 1/app2.dart
@@ -11,4 +11,12 @@ void test(){
   // * Using var keyword 
   var _age = 10 ;
   print(_age.runtimeType) ;
+
+
+  // * Using Final keyword - final keyword is like const  
+
+  final int age_ = 10 ;
+
+
+
 }
