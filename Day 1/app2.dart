01f33f969c95b8b193bcc21cf75a84768399ca8d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-05 09:41:07 +0800

    update here

--- Day 1/app2.dart
+++ Day 1/app2.dart
@@ -41,9 +41,13 @@ void for_loop() {
   for(var i = 0 ; i < 3 ; i++){
     print("Outer Loop $i") ;
 
-    innerflow
+    innerflow:
+
     for(var j = 0 ; j < 5 ; j++){
 
+      if(i == 2 ){
+        break innerflow ;
+      }
     }
   }
 
