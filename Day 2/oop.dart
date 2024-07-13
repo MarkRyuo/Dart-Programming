25649b50d2cebcf450e1751f1bf22a653ab7f4a1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-13 15:28:35 +0800

    update here

--- Day 2/oop.dart
+++ Day 2/oop.dart
@@ -2,4 +2,5 @@
 // * OOP have properties and Behaviors 
 
 // * Each object has properties and behaviors 
-// * Properties another called Attri
+// * Properties another called Attributes
+// * Behaviors another called Methods 
