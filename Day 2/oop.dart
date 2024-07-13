eea069be681e32e096031df873685167b8924765
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-13 15:29:29 +0800

    update here

--- Day 2/oop.dart
+++ Day 2/oop.dart
@@ -4,3 +4,6 @@
 // * Each object has properties and behaviors 
 // * Properties another called Attributes
 // * Behaviors another called Methods 
+
+
+// * Features of Object Oriented Programming(OOP)
