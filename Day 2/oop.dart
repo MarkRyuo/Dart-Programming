d710439cf67f82dff4595adba995dc26cb7cf10b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-13 15:38:20 +0800

    update here

--- Day 2/oop.dart
+++ Day 2/oop.dart
@@ -2,8 +2,8 @@
 // * OOP have properties and Behaviors 
 
 // * Each object has properties and behaviors 
-// * Properties another called Attributes
-// * Behaviors another called Methods 
+// * Properties another called Attributes or fields
+// * Behaviors another called Methods or functions 
 
 
 // * Features of Object Oriented Programming(OOP)
@@ -16,5 +16,7 @@
 // * 1.Class - Is the blueprint for creating Objects  
 
 class Dog {
-  
+  // Properties or fields  
+  // methods of Function
+
 }
